3fe7cff5dcb96f6bbd9ba0c7495c72a62a39193a
Shriman1527 <shrimanghugare1527@gmail.com>
2025-04-25 23:26:24 +0530

    Intial setup

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+/*
+// @ts-ignore
+// ugly way to remove the errors
+*/
+import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+
+
+
+//.d.ts
+
+const app=express();
+
+app.post("/api/v1/signup",(req,res)=>{
+
+})
+
+app.post("/api/v1/signin",(req,res)=>{
+    
+})
+
+app.post("/api/v1/content",(req,res)=>{
+    
+})
+
+app.get("/api/v1/content",(req,res)=>{
+    
+})
+
+app.delete("/api/v1/content",(req,res)=>{
+
+})
+
+app.post("/api/v1/brain/share",(req,res)=>{
+
+})
+
+app.get("/api/v1/brain/:sharelink",(req,res)=>{
+
+})
+
+
+
+
+//Vector databases embeddings
+
+//Building gpt from scratch => carpaci something 
+
+// We will reach till 1 hour of video 
+// Next part is to understand the vector data embeddings
+
+
